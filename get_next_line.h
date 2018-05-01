5c8e020a2b262fe09f3e29ffc08d73f561d8c030
Malik Jean <maljean@e1z4r10p5.42.us.org>
2018-05-01 15:45:44 -0700

    This function will be used for reading the contents of a given file

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/21 20:29:04 by maljean           #+#    #+#             */
+/*   Updated: 2018/04/24 16:14:05 by maljean          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 16
+
+int		get_next_line(int fd, char **line);
+
+#endif
